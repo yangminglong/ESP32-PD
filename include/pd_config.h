6ad164402d6606e56842ee85d009dd2fbcc7e0fe
yangminglong <ilyyangminglong@126.com>
2025-04-29 11:37:40 +0800

    增加引脚说明

--- include/pd_config.h
+++ include/pd_config.h
@@ -3,10 +3,10 @@
 #include <stdint.h>
 #include "pd_types.h"
 
+#define GPIO_PD 4  //  状态指示？
 // #define GPIO_CC1 3 // 通过与GPIO_TX连接来得到一个大约1.7V的分压器, ESP32内部上拉、下拉电阻为47K
-#define GPIO_PD 4
-#define GPIO_TX 3
-#define GPIO_CC1_IN 0
+#define GPIO_TX 3 // ^-^ 连接USB-CC. we now have configured the GPIO_CC1 pin and GPIO_TX pin to drive against each other. with these drive capabilities, we get a voltage divider to approx 1.7V, which is closer to the expected 1.1V on the PD lines
+#define GPIO_CC1_IN 0 // ^-^ 连接MUN5233集电极. PD RX Path: Converts the 0-1.2V CC signal to 3.3V using a MUN5233 transistor and the IO in weak mode as a pull up.
 
 #define PD_BUFFER_COUNT 64
 
